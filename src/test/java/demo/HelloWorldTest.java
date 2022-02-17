c2b126b6b580c6f862f331073d46d481fd149d31
Sahachok.J <sahachok.j@kbtg.tech>
2022-02-17 11:02:28 +0700

    using displayname to show the testcase topic

--- src/test/java/demo/HelloWorldTest.java
+++ src/test/java/demo/HelloWorldTest.java
@@ -1,10 +1,20 @@
 package demo;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class HelloWorldTest {
 
-    @org.junit.jupiter.api.Test
+//    @org.junit.jupiter.api.Test
+    @Test
+    @DisplayName("Test case hi")
     public void hi() {
+    }
+
+    @Test
+    @DisplayName("Test case hi2")
+    public void hi2() {
     }
 }
