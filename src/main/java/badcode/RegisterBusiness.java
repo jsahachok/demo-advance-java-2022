0ff6bef25c3e85d0f2515c8a53716b32d68b0291
Sahachok.J <sahachok.j@kbtg.tech>
2022-02-17 15:54:33 +0700

    Refactor with responsibility

--- src/main/java/badcode/RegisterBusiness.java
+++ src/main/java/badcode/RegisterBusiness.java
@@ -21,30 +21,11 @@ public class RegisterBusiness {
     }
 
     private void validationSpeaker(Speaker speaker) {
-        String[] domains = {"gmail.com", "live.com"};
-
         //Failure
         if(speaker == null){
             throw new RuntimeException("Speaker is null");
         }
-
-        if (isNullorEmpty(speaker.getFirstName())) {
-            throw new ArgumentNullException("First name is required.");
-        } else if (isNullorEmpty(speaker.getLastName())) {
-            throw new ArgumentNullException("Last name is required.");
-        } else if (isNullorEmpty(speaker.getEmail())) {
-            throw new ArgumentNullException("Email is required.");
-        }
-        // Your Tasks ...
-        String emailDomain = getEmailDomain(speaker.getEmail()); // Avoid ArrayIndexOutOfBound
-
-        if (Arrays.stream(domains).filter(it -> it.equals(emailDomain)).count() != 1) {
-            throw new SpeakerDoesntMeetRequirementsException("Speaker doesn't meet our standard rules.");
-        }
-    }
-
-    private boolean isNullorEmpty(String speaker) {
-        return speaker == null || speaker.trim().equals("");
+        speaker.validate();
     }
 
     int getFee(int experienceYear) {
@@ -59,12 +40,6 @@ public class RegisterBusiness {
             fee = 50;
         }
         return fee;
-    }
-
-    public String getEmailDomain(String email) {
-        String[] inputs = email.trim().split("@");
-        if (inputs.length == 2) return inputs[1];
-        throw new DomainEmailInvalidException("Email domain invalid.");
     }
 
 }
